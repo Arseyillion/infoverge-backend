f1ab1a484f4b5a45b0a7e3b6ead1a890a15f62bb
Arseyillion <echukwuemeka017@gmail.com>
2025-11-19 14:28:18 +0100

    telling strapi cloud to not run cloud-cronjob-runner

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+// config/plugins.js
+
+export default () => ({
+  // Disable the cloud-cronjob-runner plugin entirely
+  'cloud-cronjob-runner': {
+    enabled: false,
+  },
+
+  // Add other plugin configurations here if needed
+  // e.g.,
+  // 'email': { provider: 'sendgrid', ... }
+});
