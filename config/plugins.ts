0713396618fa9f95830bd890d66fdec62e12bc34
Arseyillion <echukwuemeka017@gmail.com>
2025-11-19 16:27:38 +0100

    here we switched from strapi v31 to strapi v30 to see if that will resolve the issues we are having with deployment

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,12 +1,1 @@
-// config/plugins.js
-
-export default () => ({
-  // Disable the cloud-cronjob-runner plugin entirely
-  'cloud-cronjob-runner': {
-    enabled: false,
-  },
-
-  // Add other plugin configurations here if needed
-  // e.g.,
-  // 'email': { provider: 'sendgrid', ... }
-});
+export default () => ({});
